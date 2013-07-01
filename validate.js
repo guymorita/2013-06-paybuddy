26cebe7bba1284c7e5cb0abf8eb4cbef3f602a18
Guy Morita <guymorita@gmail.com>
2013-07-01 00:47:35 -0700

    added basic validation on the CC# and CCV code:

--- validate.js
+++ validate.js
@@ -1,8 +1,22 @@
 $(function(){
 
   var formHandler = function(event){
-    console['log']("submitted"); // fixme: validate form here
-
+    var ccReg = /\b\d{13,16}\b/;
+    var monthReg = /\b[a-z]{3}\b/;
+    var ccvReg = /\b\d{3,4}\b/;
+    var alerts = [];
+      if (!ccReg.test(document.ccnForm.num.value)){
+        alerts.push("Please enter in a 13-16 digit credit card number");
+      }
+      if (!monthReg.test(document.ccnForm.month.value)){
+        alerts.push("Wrong month yo");
+      }
+      if (!ccvReg.test(document.ccnForm.ccv.value)){
+        alerts.push("Please put in a valid CCV");
+      }
+      if (alerts !== []){
+        alert(alerts.join("\n"));
+      }
     event['preventDefault'](); // prevents the page from reloading
   };
 
